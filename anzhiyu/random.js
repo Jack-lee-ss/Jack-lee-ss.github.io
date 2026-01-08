c605024b41bf9aa45ebcbeb9d1059024a6b7ab23
jack <lcq19920706@163.com>
2026-01-08 15:22:50 +0800

    Site updated: 2026-01-08 15:22:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/07/hello-world/"];function toRandomPost(){
+var posts=["2026/01/08/这是一篇文章/","2026/01/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
